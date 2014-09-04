c8f2e86322c321a58d17382ab1238e9e5e3e3ff8
otf <phoenixsingleton@gmail.com>
2014-09-04 22:12:34 +0900

    複雑なテストをAcceptanceTestに分離した

---
+++ Gohe.Ast.Tests/XdefAcceptanceTest.fs
@@ -0,0 +1,32 @@
+﻿module Test.``Xdef Acceptance Test``
+
+open NUnit.Framework
+open FsUnit
+
+open AstUtility
+
+[<Test>]
+let ``XdefNodeをパースできる`` () =  
+    let xdef = """
+Root
+  @Id : Guid -- ID属性
+  Description : String -- 詳細
+  Children
+    Child* : [0,10)
+  Behavior
+    OptionA? : "Enabled" """.Trim()
+
+    let expected = 
+      celm "Root" required None <| seq required [
+          attr "Id" required (Some "ID属性") Ast.Guid
+          elm "Description" required (Some "詳細") Ast.String
+          celm "Children" required None <| seq required [
+              elm "Child" many None (Ast.intRange 0 10) 
+            ] 
+          celm "Behavior" required None <| seq required [
+              elm "OptionA" optional None (Ast.FixedString "Enabled") 
+            ] 
+        ]
+
+    parse Ast.pNode xdef
+    |> should equal (Some <| expected)
--- Gohe.Ast.Tests/XdefComplexElementTest.fs
+++ Gohe.Ast.Tests/XdefComplexElementTest.fs
@@ -98,29 +98,3 @@ let ``子要素持ちのXdefElement(All)をパースできる`` () =
 
     parse Ast.pNode xdef
     |> should equal (Some <| expected)
-
-[<Test>]
-let ``複雑なXdefNodeをパースできる`` () =  
-    let xdef = """
-Root
-  @Id : Guid -- ID属性
-  Description : String -- 詳細
-  Children
-    Child* : [0,10)
-  Behavior
-    OptionA? : "Enabled" """.Trim()
-
-    let expected = 
-      celm "Root" required None <| seq required [
-          attr "Id" required (Some "ID属性") Ast.Guid
-          elm "Description" required (Some "詳細") Ast.String
-          celm "Children" required None <| seq required [
-              elm "Child" many None (Ast.intRange 0 10) 
-            ] 
-          celm "Behavior" required None <| seq required [
-              elm "OptionA" optional None (Ast.FixedString "Enabled") 
-            ] 
-        ]
-
-    parse Ast.pNode xdef
-    |> should equal (Some <| expected)
