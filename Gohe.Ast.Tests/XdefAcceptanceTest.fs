0d60fbaf93eb319714f37030eb6cd48bb0ff3800
otf <phoenixsingleton@gmail.com>
2014-09-04 22:20:09 +0900

    順序インジケータ指定の受け入れテストを分離

--- Gohe.Ast.Tests/XdefAcceptanceTest.fs
+++ Gohe.Ast.Tests/XdefAcceptanceTest.fs
@@ -10,28 +10,33 @@ let ``XdefNodeをパースできる`` () =
     let xdef = """
 Root
   @Id : Guid -- ID属性
-  Description : String -- 詳細
   Children
-    Child* : [0,10)
-  MustSeq* :: Sequence{0..10}
-  MustChoice* :: Choice{0..10}
-  MustAll* :: All{0..10}
-  Behavior
-    OptionA? : "Enabled" """.Trim()
+    Child* : [0,10)""".Trim()
 
     let expected = 
       celm "Root" required None <| seq required [
           attr "Id" required (Some "ID属性") Ast.Guid
-          elm "Description" required (Some "詳細") Ast.String
           celm "Children" required None <| seq required [
               elm "Child" many None (Ast.intRange 0 10) 
             ] 
+        ]
+
+    parse Ast.pNode xdef
+    |> should equal (Some <| expected)
+
+[<Test>]
+let ``明示的に順序インジケータが指定されたXdefNodeをパースできる`` () =  
+    let xdef = """
+Root
+  MustSeq* :: Sequence{0..10}
+  MustChoice* :: Choice{0..10}
+  MustAll* :: All{0..10}""".Trim()
+
+    let expected = 
+      celm "Root" required None <| seq required [
           celm "MustSeq" many None <| seq (specific 0 10) [ ] 
           celm "MustChoice" many None <| choice (specific 0 10) [ ] 
           celm "MustAll" many None <| all (specific 0 10) [ ] 
-          celm "Behavior" required None <| seq required [
-              elm "OptionA" optional None (Ast.FixedString "Enabled") 
-            ] 
         ]
 
     parse Ast.pNode xdef
