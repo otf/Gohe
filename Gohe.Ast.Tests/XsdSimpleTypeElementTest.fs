06b34628f1bee7eb8f1484bbb2e175fc0e125dca
otf <phoenixsingleton@gmail.com>
2014-09-06 23:28:37 +0900

    string以外のprimitiveTypeのxsd化を実装

---
+++ Gohe.Ast.Tests/XsdSimpleTypeElementTest.fs
@@ -0,0 +1,28 @@
+﻿module Test.``Xsd SimpleType Element Test``
+
+open NUnit.Framework
+open FsUnit
+
+open System.Xml
+open System.Xml.Schema
+open XdefUtility
+open Xsd
+
+let inline name (x:^a) = ((^a) : (member Name : string) x)
+let inline typeNameOf (x:^a) = ((^a) : (member SchemaTypeName : XmlQualifiedName) x)
+
+let primitiveTypeTestCases : obj [][] = [|
+  [|Xdef.Bool; "boolean"|]
+  [|Xdef.String; "string"|]
+  [|Xdef.Int; "integer"|]
+  [|Xdef.Float; "float"|]
+  [|Xdef.Decimal; "decimal"|]
+//  [|Xdef.Guid, ""|]
+|]
+
+[<TestCaseSource("primitiveTypeTestCases")>]
+let ``PrimitiveTypeの要素をXsd化できる`` inputType expected = 
+  let input = elm "Root" required None inputType
+  
+  Xsd.fromNode input |> name |> should equal "Root"
+  Xsd.fromNode input |> typeNameOf |> should equal (XmlQualifiedName(expected, "http://www.w3.org/2001/XMLSchema"))
---
+++ Gohe.Ast/Xsd.fs
@@ -0,0 +1,28 @@
+﻿module Xsd
+
+open System.Xml
+open System.Xml.Schema
+open FParsec
+
+open Xdef
+
+let simpleTypeToQName etype = 
+  let qName nm = XmlQualifiedName(nm, "http://www.w3.org/2001/XMLSchema")
+  match etype with
+  | Bool -> qName "boolean"
+  | String -> qName "string"
+  | Int -> qName "integer"
+  | Float -> qName "float"
+  | Decimal -> qName "decimal"
+  | _ -> failwith "unsupported type"
+
+let fromElement  ({ Name = name; Type = Simple eType } : Element) = 
+  let result = XmlSchemaElement()
+  result.Name <- name
+  result.SchemaTypeName <- simpleTypeToQName eType
+  result
+
+let fromNode node = 
+  match node with
+  | Element element -> fromElement element 
+  | _ -> failwith ""
