0df82c0076c17354d23a0acefdd75d643ec43be7
otf <phoenixsingleton@gmail.com>
2014-09-06 12:09:41 +0900

    子要素のテスト要因を複雑型要素のテストに追加した

--- Gohe.Ast.Tests/XdefComplexElementTest.fs
+++ Gohe.Ast.Tests/XdefComplexElementTest.fs
@@ -32,53 +32,23 @@ let occursTestFactors = [
   "{100..}", min 100
 ]
 
-let orderAndOccursTestCases : obj [][] = [|
+let childrenTestFactors = [
+  "", []
+  "\n  Child : String", [elm "Child" required None Ast.String]
+  "\n  @Child : String", [attr "Child" required None Ast.String]
+  "\n  @Child1 : String\n  Child2 : String", [attr "Child1" required None Ast.String; elm "Child2" required None Ast.String]
+  "\n  Child1 : String\n  Child2 : String", [elm "Child1" required None Ast.String; elm "Child2" required None Ast.String]
+]
+
+let complexTypeTestCases : obj [][] = [|
   for (orderInput, orderExpected) in orderTestFactors do
   for (occursInput, occursExpected) in occursTestFactors do
-    yield [| orderInput; orderExpected; occursInput; occursExpected |]
+  for (childrenInput, childrenExpected) in childrenTestFactors do
+    yield [| orderInput; orderExpected; occursInput; occursExpected; childrenInput; childrenExpected |]
 |]
 
-[<TestCaseSource("orderAndOccursTestCases")>]
-let ``順序インジケータと出現回数指定された複雑型XdefElementをパースできる`` complexTypeInput complexTypeExpected occursInput occursExpected =
-    let expected = celm "Root" required None <| complexTypeExpected (occursExpected: Ast.XdefOccurrence) ([] : Ast.XdefNode list)
-    parse Ast.pNode (sprintf "Root :: %s%s" complexTypeInput occursInput)
+[<TestCaseSource("complexTypeTestCases")>]
+let ``複雑型のXdefElementをパースできる`` complexTypeInput complexTypeExpected occursInput occursExpected childrenInput childrenExpected =
+    let expected = celm "Root" required None <| complexTypeExpected (occursExpected: Ast.XdefOccurrence) childrenExpected
+    parse Ast.pNode (sprintf "Root :: %s%s%s" complexTypeInput occursInput childrenInput)
     |> should equal (Some expected)
-
-[<Test>]
-let ``子要素持ちのXdefElement(Sequence)をパースできる`` () =  
-    let xdef = "Root\n  @Name : String\n  Description : String"
-
-    let expected = 
-      celm "Root" required None <| seq required [
-          attr "Name" required  None Ast.String
-          elm "Description" required None Ast.String
-        ]
-
-    parse Ast.pNode xdef
-    |> should equal (Some <| expected)
-
-[<Test>]
-let ``子要素持ちのXdefElement(Choice)をパースできる`` () =  
-    let xdef = "AxorB :: Choice\n  A : String\n  B : String"
-
-    let expected = 
-      celm "AxorB" required None <| choice required [
-          elm "A" required None Ast.String
-          elm "B" required None Ast.String
-        ]
-
-    parse Ast.pNode xdef
-    |> should equal (Some <| expected)
-
-[<Test>]
-let ``子要素持ちのXdefElement(All)をパースできる`` () =  
-    let xdef = "AorB :: All\n  A : String\n  B : String"
-
-    let expected = 
-      celm "AorB" required None <| all required [
-          elm "A" required None Ast.String
-          elm "B" required None Ast.String
-        ]
-
-    parse Ast.pNode xdef
-    |> should equal (Some <| expected)
