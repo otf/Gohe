e1f5884c26f23e2ee67029a1c459dd519361bc13
otf <phoenixsingleton@gmail.com>
2014-09-06 23:43:55 +0900

    FixedValue系のprimitiveTypeのxsd化を実装

--- Gohe.Ast.Tests/XsdSimpleTypeElementTest.fs
+++ Gohe.Ast.Tests/XsdSimpleTypeElementTest.fs
@@ -10,6 +10,7 @@ open Xsd
 
 let inline name (x:^a) = ((^a) : (member Name : string) x)
 let inline typeNameOf (x:^a) = ((^a) : (member SchemaTypeName : XmlQualifiedName) x)
+let inline fixedValue (x:^a) = ((^a) : (member FixedValue : string) x)
 
 let primitiveTypeTestCases : obj [][] = [|
   [|Xdef.Bool; "boolean"|]
@@ -20,9 +21,22 @@ let primitiveTypeTestCases : obj [][] = [|
 //  [|Xdef.Guid, ""|]
 |]
 
+let fixedTypeTestCases : obj [][] = [|
+  [|Xdef.FixedString("hello"); "hello"|]
+  [|Xdef.FixedInt(100); "100"|]
+  [|Xdef.FixedFloat(100.001); "100.001"|]
+|]
+
 [<TestCaseSource("primitiveTypeTestCases")>]
 let ``PrimitiveTypeの要素をXsd化できる`` inputType expected = 
   let input = elm "Root" required None inputType
   
   Xsd.fromNode input |> name |> should equal "Root"
   Xsd.fromNode input |> typeNameOf |> should equal (XmlQualifiedName(expected, "http://www.w3.org/2001/XMLSchema"))
+
+[<TestCaseSource("fixedTypeTestCases")>]
+let ``PrimitiveType(Fixed)の要素をXsd化できる`` inputType expected = 
+  let input = elm "Root" required None inputType
+  
+  Xsd.fromNode input |> name |> should equal "Root"
+  Xsd.fromNode input |> fixedValue |> should equal expected
--- Gohe.Ast/Xsd.fs
+++ Gohe.Ast/Xsd.fs
@@ -6,20 +6,33 @@ open FParsec
 
 open Xdef
 
-let simpleTypeToQName etype = 
-  let qName nm = XmlQualifiedName(nm, "http://www.w3.org/2001/XMLSchema")
+type private ElementType = 
+  | QName of XmlQualifiedName
+  | FixedValue of string
+
+let private fromSimpleType etype = 
+  let qName nm = QName <| XmlQualifiedName(nm, "http://www.w3.org/2001/XMLSchema")
   match etype with
   | Bool -> qName "boolean"
   | String -> qName "string"
   | Int -> qName "integer"
   | Float -> qName "float"
   | Decimal -> qName "decimal"
+  | FixedString value -> FixedValue value
+  | FixedInt value -> FixedValue <| value.ToString()
+  | FixedFloat value -> FixedValue <| value.ToString()
   | _ -> failwith "unsupported type"
 
-let fromElement  ({ Name = name; Type = Simple eType } : Element) = 
+let fromElement  ({ Name = name; Type = Simple sType } : Element) = 
   let result = XmlSchemaElement()
   result.Name <- name
-  result.SchemaTypeName <- simpleTypeToQName eType
+
+  match fromSimpleType sType with
+  | QName qname -> 
+      result.SchemaTypeName <- qname
+  | FixedValue value ->
+      result.FixedValue <- value
+
   result
 
 let fromNode node = 
