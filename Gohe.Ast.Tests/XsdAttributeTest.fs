d3dd246458050c0f44a8be6d95d5e727059916c7
otf <phoenixsingleton@gmail.com>
2014-09-15 20:05:59 +0900

    組み込み型の属性をXsd化できることのテストを追加

--- Gohe.Ast.Tests/XsdAttributeTest.fs
+++ Gohe.Ast.Tests/XsdAttributeTest.fs
@@ -9,6 +9,27 @@ open XdefUtility
 open Xsd
 open XsdUtility
 
+let schemaQName nm =  XmlQualifiedName(nm, "http://www.w3.org/2001/XMLSchema")
+let primitiveTypeTestCases : obj [][] = [|
+  [|Xdef.TypeRef("boolean"); schemaQName "boolean"|]
+  [|Xdef.TypeRef("byte"); schemaQName "byte"|]
+  [|Xdef.TypeRef("string"); schemaQName "string"|]
+  [|Xdef.TypeRef("int"); schemaQName "int"|]
+  [|Xdef.TypeRef("float"); schemaQName "float"|]
+  [|Xdef.TypeRef("decimal"); schemaQName "decimal"|]
+  [|Xdef.TypeRef("date"); schemaQName "date"|]
+  [|Xdef.TypeRef("time"); schemaQName "time"|]
+  [|Xdef.TypeRef("dateTime"); schemaQName "dateTime"|]
+  [|Xdef.TypeRef("duration"); schemaQName "duration"|]
+|]
+
+[<TestCaseSource("primitiveTypeTestCases")>]
+let ``PrimitiveTypeの属性をXsd化できる`` inputType expected = 
+  let input = attr "Attr" useRequired None inputType
+  
+  Xsd.fromNode "" input |> asAttr  |> name |> should equal "Attr"
+  Xsd.fromNode "" input |> asAttr  |> typeNameOf |> should equal expected
+
 let fixedTypeTestCases : obj [][] = [|
   [|Xdef.FixedBoolean(true); "true"|]
   [|Xdef.FixedByte(100y); "100"|]
