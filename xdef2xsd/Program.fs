293a78be3474c32ff420d78cb63e92b617e28b6c
otf <phoenixsingleton@gmail.com>
2014-12-15 23:06:16 +0900

    xdef2xsdに-iと-oオプションを追加(エンコーディングはUTF-8) refs #4 #5

--- xdef2xsd/Program.fs
+++ xdef2xsd/Program.fs
@@ -1,21 +1,57 @@
 ﻿open System
+open System.IO
+open System.Text
 open System.Xml
 open System.Xml.Schema
 open FParsec
+open Nessos.UnionArgParser
 
-let outputXsd (schema:XmlSchema) =
+let outputXsd (writer:TextWriter) (schema:XmlSchema) =
   let nsmgr = XmlNamespaceManager(NameTable())
   if schema.TargetNamespace <> null then
     nsmgr.AddNamespace("", schema.TargetNamespace)
   nsmgr.AddNamespace("xs", "http://www.w3.org/2001/XMLSchema")
-  schema.Write(Console.Out, nsmgr)
+  schema.Write(writer, nsmgr)
+
+type Arguments =
+  | [<First; AltCommandLine("-i")>]  Input of string
+  | [<AltCommandLine("-o")>] Output of string
+with
+  interface IArgParserTemplate with
+    member s.Usage =
+      match s with
+      | Input _ -> "specify a input file."
+      | Output _ -> "specify a output file."
+
+let parser = UnionArgParser.Create<Arguments>()
 
 [<EntryPoint>]
 let main argv = 
-  match Console.In.ReadToEnd() |> Xdef.parse with
-  | Success (r, s, p) -> 
-      Xsd.fromSchema r |> outputXsd
-      0
-  | Failure (msg, err, s) -> 
-      eprintfn "%s" msg
+  let args =
+    try 
+      parser.Parse(argv) |> Some
+    with _ ->
+      None
+
+  match args with
+  | Some args ->
+      let input =
+        match args.TryGetResult (<@ Input @>) with
+        | Some inputPath -> File.ReadAllText(inputPath, Encoding.UTF8)
+        | None -> Console.In.ReadToEnd()
+
+      let output =
+        match args.TryGetResult (<@ Output @>) with
+        | Some outputPath -> new StreamWriter(File.OpenWrite(outputPath), Encoding.UTF8) :> TextWriter
+        | None -> Console.Out
+
+      match input |> Xdef.parse with
+      | Success (r, s, p) -> 
+          Xsd.fromSchema r |> outputXsd output
+          0
+      | Failure (msg, err, s) -> 
+          eprintfn "%s" msg
+          -1
+  | None ->
+      printfn "%s" <| parser.Usage()
       -1
