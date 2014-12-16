6db4ae9bdf9213cba2f947400924bc51c1545067
k.hamano <k.hamano@otr-jp.com>
2014-12-16 10:00:12 +0900

    add script refs #6 #7

---
+++ xdef2xsd/xdef2xsd.ps1
@@ -0,0 +1,21 @@
+﻿Param(
+[System.String]$input_dir,
+[System.String]$output_dir
+)
+[System.String]$input_fx = "xdef"
+
+function OutputXsd($file)
+{
+	Add-Type -Assembly System.IO
+    $out_path = [System.IO.Path]::Combine($output_dir, $file.BaseName + ".xsd")
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = "xdef2xsd.exe"
+    $pinfo.Arguments = @("-i"; $file.FullName; "-o"; $out_path)
+    $p = New-Object System.Diagnostics.Process
+    $p.StartInfo = $pinfo
+    $p.Start() | Out-Null
+    $p.WaitForExit()
+}
+
+Get-ChildItem ($input_dir + "\*." + $input_fx) | ForEach-Object { OutputXsd($_) }
+
