e25ef520987d3effe47325208e56764272b5f98e
k.hamano <k.hamano@otr-jp.com>
2014-12-16 11:14:10 +0900

    add nuspec and fix script refs #6 #7

--- xdef2xsd/xdef2xsd.ps1
+++ xdef2xsd/xdef2xsd.ps1
@@ -1,6 +1,7 @@
 ﻿Param(
 [System.String]$input_dir,
-[System.String]$output_dir
+[System.String]$output_dir,
+[System.String]$tool_path
 )
 [System.String]$input_fx = "xdef"
 
@@ -9,8 +10,9 @@ function OutputXsd($file)
 	Add-Type -Assembly System.IO
     $out_path = [System.IO.Path]::Combine($output_dir, $file.BaseName + ".xsd")
     $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-    $pinfo.FileName = "xdef2xsd.exe"
+    $pinfo.FileName = [System.IO.Path]::Combine($tool_path, "xdef2xsd.exe")
     $pinfo.Arguments = @("-i"; $file.FullName; "-o"; $out_path)
+	$pinfo.UseShellExecute = $false
     $p = New-Object System.Diagnostics.Process
     $p.StartInfo = $pinfo
     $p.Start() | Out-Null
@@ -18,4 +20,3 @@ function OutputXsd($file)
 }
 
 Get-ChildItem ($input_dir + "\*." + $input_fx) | ForEach-Object { OutputXsd($_) }
-
